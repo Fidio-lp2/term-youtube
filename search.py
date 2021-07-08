40aa087937e94e573e2ba7ba297ebab053e6fa02
Fidio-lp2 <qdatian9@gmail.com>
2021-07-09 01:20:21 +0900

    the middle of appending docstring💫

--- search.py
+++ search.py
@@ -1,67 +1,83 @@
-from googleapiclient.discovery import build
-from typing import Final
-import json
+# -*- coding: utf-8 -*-
+""" module docstring
 
+
+"""
+
+import json
+from typing import Final
+from googleapiclient.discovery import build
+
+# youtube api token is read from other file ignored .gitignore
 with open(".token.json", "r") as tokenFile:
     KEY: Final[str] = (json.load(tokenFile))["youtubeApiToken"]
 HEADER: Final[str] = "https://www.youtube.com/watch?v="
 
-def get_movie_data(search_name, movie_num):
+def get_video_data(search_name, video_num):
     """
-    get data of movie in youtube in like json
+    Get data of video in youtube
+
+
 
     Parameters
     ----------
     search_name : str
-        
-    movie_num : int
-        
+        video name you want information of video
+    video_num : int
+        nunmber of video information you want
+
+    Returns
+    -------
+    response : resource
+        data of video in like json file format
     """
 
     query: str = search_name
-    movie_cnt: int = movie_num + 5
+    video_cnt: int = video_num + 5
     youtube: resource = build("youtube","v3",developerKey=KEY)
     response = youtube.search().list(
                 q=query,
                 part="id,snippet",
-                maxResults = movie_cnt
+                maxResults = video_cnt
     ).execute()
 
     i: int = 0
     while i < len(response["items"]):
         try:
-            video_id: str = response["items"][i]["id"]["videoId"]
+            response["items"][i]["id"]["videoId"]
         except KeyError:
             response["items"].pop(i)
             continue
         i+=1
 
     while True:
-        if len(response["items"]) <= movie_num:
-            break
-        elif len(response["items"]) > movie_num:
+        if len(response["items"]) > video_num:
             response["items"].pop(len(response["items"])-1)
+        elif len(response["items"]) <= video_num:
+            break
 
     return response
 
-# return single movie name
-def get_movie_name(search_name):
-    response = get_movie_data(search_name, 1)
-    
+def get_video_name(search_name):
+    """
+    get video name 
+    """
+
+    response = get_video_data(search_name, 1)
+ 
     name = response["items"][0]["snippet"]["title"]
 
     return name
 
-# return movie name list
-def get_movie_names(search_name, movie_num):
-    response = get_movie_data(search_name, movie_num)
+def get_video_names(search_name, video_num):
+    response = get_video_data(search_name, video_num)
     
     names = [item["snippet"]["title"] for item in response["items"]]
 
     return names
 
-# return movie url
-def get_movie_url(search_name):
-    datas = get_movie_data(search_name, 1)
+# return video url
+def get_video_url(search_name):
+    datas = get_video_data(search_name, 1)
     
     return HEADER + datas["items"][0]["id"]["videoId"]
