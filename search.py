b13d2f7e50a6ccfea3cebfabfe32cb51629b4a28
Fidio-lp2 <qdatian9@gmail.com>
2021-07-09 11:16:27 +0900

    Finished writting docstring🌟

--- search.py
+++ search.py
@@ -1,40 +1,42 @@
 # -*- coding: utf-8 -*-
-""" module docstring
-
+"""
+Get information of movie in youtube
 
 """
-
 import json
 from typing import Final
 from googleapiclient.discovery import build
 
+
 # youtube api token is read from other file ignored .gitignore
 with open(".token.json", "r") as tokenFile:
-    KEY: Final[str] = (json.load(tokenFile))["youtubeApiToken"]
-HEADER: Final[str] = "https://www.youtube.com/watch?v="
+    YOUTUBE_API_TOKEN: Final[str] = (json.load(tokenFile))["youtubeApiToken"]
+URL_HEADER: Final[str] = "https://www.youtube.com/watch?v="
+
 
 def get_video_data(search_name, video_num):
     """
     Get data of video in youtube
 
-
+    This method is take information of videos in youtube with google
+    youtube api. Other methods in this module, search module use this method.
+    So if you delete this method, they will not be usable.
 
     Parameters
     ----------
     search_name : str
-        video name you want information of video
+        Video name you want information of video.
     video_num : int
-        nunmber of video information you want
+        Nunmber of video information you want.
 
     Returns
     -------
     response : resource
-        data of video in like json file format
+        Data of video in like json file format.
     """
-
     query: str = search_name
     video_cnt: int = video_num + 5
-    youtube: resource = build("youtube","v3",developerKey=KEY)
+    youtube: resource = build("youtube","v3",developerKey=YOUTUBE_API_TOKEN)
     response = youtube.search().list(
                 q=query,
                 part="id,snippet",
@@ -60,24 +62,59 @@ def get_video_data(search_name, video_num):
 
 def get_video_name(search_name):
     """
-    get video name 
-    """
+    Get video title on youtube come on the top after searching.
 
+    Parameters
+    ----------
+    search_name : str
+        Video name you want.
+
+    Returns
+    -------
+    name : str
+        Video title you want.
+    """
     response = get_video_data(search_name, 1)
- 
+
     name = response["items"][0]["snippet"]["title"]
 
     return name
 
 def get_video_names(search_name, video_num):
+    """
+    Get a list of video title.
+
+    Parameters
+    ----------
+    search_name : str
+        Video name you want.
+    video_num : int
+        Number of the video title you want.
+
+    Returns
+    -------
+    names : list[str]
+        List of video title.
+    """
     response = get_video_data(search_name, video_num)
-    
+
     names = [item["snippet"]["title"] for item in response["items"]]
 
     return names
 
-# return video url
 def get_video_url(search_name):
+    """
+    Get a url of video in youtube come on the top after searching.
+
+    Parameters
+    ----------
+    search_name : str
+        Video name you want url.
+
+    Returns
+    -------
+    Video url.
+    """
     datas = get_video_data(search_name, 1)
-    
-    return HEADER + datas["items"][0]["id"]["videoId"]
+
+    return URL_HEADER + datas["items"][0]["id"]["videoId"]
