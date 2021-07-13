d666ee898afae03bbd98c2da15b520ee3cb3fd42
Fidio-lp2 <qdatian9@gmail.com>
2021-07-13 20:33:30 +0900

    Successful for implementation of the basic operation🌟

---
+++ term-youtube/__main__.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+"""
+Main script...!
+
+"""
+from search import *
+from stream import *
+
+
+def main():
+    player = MusicStreamer()
+
+    player.add_song("https://www.youtube.com/watch?v=WeH2nuoQcYk")
+    player.add_song("https://youtu.be/aCxOn3Pfljg")
+    player.add_song("https://www.youtube.com/watch?v=5PTrZp5JJjk")
+
+    player.play()
+
+    key: str
+
+    while True:
+        key = input()
+
+        if key == "pause":
+            player.pause()
+
+        elif key == "next":
+            player.next()
+
+        elif key == "brevious":
+            player.previous()
+
+        elif key == "start":
+            player.play()
+
+        elif key == "stop":
+            player.stop()
+
+if __name__ == "__main__":
+    main()
--- term-youtube/stream.py
+++ term-youtube/stream.py
@@ -10,23 +10,50 @@ import pafy
 class MusicStreamer:
 
     mediaList: vlc.MediaList
-    player: vlc.MediaPlayer
+    player: vlc.MediaListPlayer
 
     def __init__(self):
-        self.mediaList= vlc.MediaList()
-        self.player = vlc.MediaPlayer()
+        self.mediaList = vlc.MediaList()
+        self.player = vlc.MediaListPlayer()
         self.player.set_media_list(self.mediaList)
 
+    # wrapper methods
     def play(self):
+        """
+        Play streaming.
+        """
         self.player.play()
 
     def stop(self):
+        """
+        Stop streaming.
+        """
         self.player.stop()
 
     def next(self):
+        """
+        Go to the next song.
+        """
         self.player.next()
 
-    def 
+    def previous(self):
+        """
+        Go back to the previous song.
+        """
+        self.player.previous()
 
-    def add_music(self, *urls):
-        
+    def pause(self):
+        """
+        Pause streaming. (toggle)
+        """
+        self.player.pause()
+
+    def add_song(self, *urls):
+        """
+        Add song.
+        """
+        videos = [pafy.new(url) for url in urls]
+        bests = [video.getbestaudio() for video in videos]
+        playurls = [best.url for best in bests]
+        for mrl in playurls:
+            self.mediaList.add_media(mrl)
